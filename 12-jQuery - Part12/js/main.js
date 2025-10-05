b0fc3f4aa6b323bde46c31060c49aa081b5a150d
ShihabHasan1 <shihabhasan685@gmail.com>
2025-10-05 15:21:32 +0600

    Lesson-12

---
+++ 12-jQuery - Part12/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.btn1').click(function(){
+        alert('Text: ' + $('#lorem').text());
+    });
+     $('.btn2').click(function(){
+        alert('Text: ' + $('#lorem').html());
+    });
+});
